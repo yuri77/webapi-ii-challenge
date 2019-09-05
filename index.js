800616398a18e9ab85c5a77367953b8c0f0b9b33
yurika sakae <yurika.sun@gmail.com>
2019-09-04 20:59:19 -0700

    server setup

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const postsRouter = require("./post/postsRouter.js");
+
+const server = express();
+server.use(express.json());
+server.use("api/posts", postsRouter);
+
+server.listen(5000, () => console.log("server on 5000"));
