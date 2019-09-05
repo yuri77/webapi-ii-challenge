aff40d844b6cf3370eb4f61138163adfb39d4ec2
yurika sakae <yurika.sun@gmail.com>
2019-09-04 21:37:07 -0700

    get posts endpoint complete

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const postsRouter = require("./post/postsRouter.js");
 
 const server = express();
 server.use(express.json());
-server.use("api/posts", postsRouter);
+server.use("/api/posts", postsRouter);
 
-server.listen(5000, () => console.log("server on 5000"));
+server.listen(5000, () => console.log("server listening on port 5000"));
--- post/postsRouter.js
+++ post/postsRouter.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const db = require("../data/db.js");
+
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("working");
+  db.find()
+    .then(posts => res.status(200).json(posts))
+    .catch(err => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved." });
+    });
 });
 
 module.exports = router;
