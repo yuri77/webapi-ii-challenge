f7bc8e8ce6d33e6512cf362fc0ff5d19d7c728ef
yurika sakae <yurika.sun@gmail.com>
2019-09-04 22:26:07 -0700

    getpost/:id endpoint complete

--- post/postsRouter.js
+++ post/postsRouter.js
@@ -14,4 +14,30 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  db.findById(id)
+    .then(([post]) => {
+      //const [post] = posts;
+      // const post = posts[0];
+      console.log(post);
+
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved." });
+    });
+});
+
+router.get;
+
 module.exports = router;
